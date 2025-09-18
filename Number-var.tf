abd6ee9107070a5217ba518346e7d98b491a185d
Chethan-05 <chetanarikela@gmail.com>
2025-09-18 23:54:24 +0530

    Create Number-var.tf

---
+++ Number-var.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "APP" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    availability_zone = var.availability_zone
+    count = var.instance_count
+   
+}
+
+
+variable "ami_id" {
+    description = "Storing AMI value"
+    type = string
+    default = "ami-0b09ffb6d8b58ca91"
+  
+}
+
+variable "instance_type" {
+    description = "Storing Instance type"
+    type = string
+    default = "t2.micro"
+  
+}
+
+variable "availability_zone" {
+    description = "Storing availability zone "
+    type = string
+    default = "us-east-1a"
+  
+}
+
+variable "instance_count" {
+    description = "Storing instance_count values"
+    type = string
+    default = 4
+  
+}
